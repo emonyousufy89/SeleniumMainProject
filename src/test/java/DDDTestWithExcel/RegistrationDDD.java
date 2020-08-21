4f79c8fcf39c127862b617e02d6304d1966cff3c
emon yousufy <emonyousufy89@gmail.com>
2020-08-20 22:56:53 -0400

    to show you guys

--- src/test/java/DDDTestWithExcel/RegistrationDDD.java
+++ src/test/java/DDDTestWithExcel/RegistrationDDD.java
@@ -12,16 +12,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-
-
-
-
-
-
 //input[@value='Male']
 //input[@value='Cricket']
-
-
 
 public class RegistrationDDD {
 
